f3f850348f7a2055439bd1cb22c1251fc719637b
Andrea S <andrea.sorcinelli@nyu.edu>
2018-10-09 14:53:21 -0400

    Update PoliceProject_Graphs_180430.R

--- PoliceProject_Graphs_180430.R
+++ PoliceProject_Graphs_180430.R
@@ -22,16 +22,3 @@ Police_Killings_By_Race_Subset_2017
 #Saving graph as pdf
 ggsave("Police_Killings_Over_Time_2017.pdf", Police_Killings_By_Race_Subset_2017, width = 7, height = 5, dpi = 300)
 
-#Creating subset of data including 2018
-Police_Killings_By_Race_Subset_2018 <- subset(Police_Killings_By_Race, Year >= 2013 & Year <= 2018)
-summary(Police_Killings_By_Race_Subset_2018)
-
-#Time course plot graphing total deaths per year by race
-Police_Killings_By_Race_Subset_2018 <- ggplot(Police_Killings_By_Race_Subset_2018, aes(Year, Police_Killings, linetype=Race)) + stat_summary(fun.y=mean, geom="line") + stat_summary(fun.data=mean_se, geom="ribbon", color=NA, alpha=.3) + theme_bw(base_size = 20) + xlab("Year") + ylab("Police Killings") + theme( legend.background=element_rect(color="black", fill="white"), legend.position = "top") + scale_linetype_manual(values=c("solid", "dashed", "dotted", "dotdash")) + scale_x_continuous(breaks=c(2013,2014,2015,2016,2017,2018)) + expand_limits(x=c(2013,2018)) + scale_y_continuous(breaks=c(400,500,600,700,800)) + expand_limits(y=c(350,800))
-
-#Returning graph
-Police_Killings_By_Race_Subset_2018 
-
-#Saving graph as pdf
-ggsave("Police_Killings_Over_Time_2018.pdf", Police_Killings_By_Race_Subset_2018, width = 7, height = 5, dpi = 300)
-
